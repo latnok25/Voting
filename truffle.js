7d327544e2e906e299dfc810207af0c66d311a9a
Danny <deromyoha@gmail.com>
2018-07-09 15:41:16 +0100

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -1,35 +1,4 @@
-/*var HDWalletProvider = require("truffle-hdwallet-provider");
 
-
-var infura_apikey ="https://ropsten.infura.io/Z7zXWLWqDGEVnOpsg0OH";
-var mnemonic = "nice toddler guard sound start across volcano fat bracket museum answer lake";
-var address = "0x23C0C9CeFd64444a1D45e76019C12b2A7A6d3190"
-
-module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
-  solc: {
-    optimizer: {
-      enabled: true,
-      runs: 200
-    }
-  },
-  networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
-    },
-    ropsten: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"+infura_apikey)
-      },
-      network_id: 3,
-      from: address,
-      gas: 4700388
-    }
-  }
-};*/
 
 let secrets = require('./secret.js');
 const WalletProvider = require("truffle-wallet-provider");
